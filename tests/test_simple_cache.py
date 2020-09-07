f62e6342d2f8e6fd09d84cf834a5921bb6c4b07b
northernSage <gfvante@gmail.com>
2020-09-07 01:33:35 -0300

    change super() for self

--- tests/test_simple_cache.py
+++ tests/test_simple_cache.py
@@ -11,25 +11,25 @@ class TestSimpleCache(CommonTests):
 
     def test_has(self):
         cache = self.cache_factory()
-        for k, v in super().sample_pairs.items():
+        for k, v in self.sample_pairs.items():
             assert cache.set(k, v)
             assert cache.has(k)
             assert not cache.has(f"{k}-unknown")
 
     def test_threshold(self):
-        threshold = len(super().sample_pairs) // 2
+        threshold = len(self.sample_pairs) // 2
         cache = self.cache_factory(threshold=threshold)
-        assert cache.set_many(super().sample_pairs)
+        assert cache.set_many(self.sample_pairs)
         assert abs(len(cache._cache) - threshold) <= 1
 
     def test_prune_old_entries(self):
-        threshold = 2 * len(super().sample_pairs) - 1
+        threshold = 2 * len(self.sample_pairs) - 1
         cache = self.cache_factory(threshold=threshold)
-        for k, v in super().sample_pairs.items():
+        for k, v in self.sample_pairs.items():
             assert cache.set(f"{k}-t0.2", v, timeout=0.2)
             assert cache.set(f"{k}-t1.0", v, timeout=1.0)
         sleep(0.3)
-        for k, v in super().sample_pairs.items():
+        for k, v in self.sample_pairs.items():
             assert cache.set(k, v)
             assert f"{k}-t1.0" in cache._cache.keys()
             assert f"{k}-t0.2" not in cache._cache.keys()
