3fdb7d87ddf7494c74cff27629d9eb56279233f1
northernSage <gfvante@gmail.com>
2020-09-07 18:56:54 -0300

    adjust test sleep time

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ class CommonTests:
     def cache_factory(self, *args, **kwargs):
         raise NotImplementedError()
 
-    sample_numbers = [-50, -25, 25, 50]
+    sample_numbers = [0, 10, 1024000, 9, 5000000000000, 99, 738, 2000000]
 
     sample_pairs = {
         128: False,
@@ -64,23 +64,20 @@ class CommonTests:
     def test_inc_dec(self):
         cache = self.cache_factory()
         for n in self.sample_numbers:
-            assert cache.inc(f"{n}-key", n)
-            assert cache.get(f"{n}-key") == n
-            assert cache.dec(f"{n}-key", 1)
-            assert cache.get(f"{n}-key") == n - 1
+            assert cache.inc(f"{n}-key-inc", n) == n
+            assert cache.get(f"{n}-key-inc") == n
+            assert cache.dec(f"{n}-key-dec", n) == -n
+            assert cache.get(f"{n}-key-dec") == -n
+            assert cache.dec(f"{n}-key-inc", 5) == n - 5
 
     def test_expiration(self):
         cache = self.cache_factory()
         for k, v in self.sample_pairs.items():
             cache.set(f"{k}-t0", v, timeout=0)
-            cache.set(f"{k}-t0.20", v, timeout=0.20)
-            cache.set(f"{k}-t0.5", v, timeout=0.5)
-        sleep(0.25)
+            cache.set(f"{k}-t0.1", v, timeout=0.1)
+            cache.set(f"{k}-t5.0", v, timeout=5.0)
+        sleep(1)
         for k, v in self.sample_pairs.items():
             assert cache.get(f"{k}-t0") == v
-            assert not cache.get(f"{k}-t0.20")
-            assert cache.get(f"{k}-t0.5") == v
-        sleep(0.3)
-        for k, v in self.sample_pairs.items():
-            assert cache.get(f"{k}-t0") == v
-            assert not cache.get(f"{k}-t0.5")
+            assert cache.get(f"{k}-t5.0") == v
+            assert not cache.get(f"{k}-t0.1")
