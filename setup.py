ea33c44cfab45d6bcbb6731a75d24109485c1212
northernSage <gfvante@gmail.com>
2021-05-22 00:15:19 -0300

    add setup.cfg

--- setup.py
+++ setup.py
@@ -1,42 +1,10 @@
 #!/usr/bin/env python
-import io
 import re
 
 from setuptools import setup
 
-with open("README.rst", "rt", encoding="utf8") as f:
-    readme = f.read()
+with open("src/cachelib/__init__.py", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
-with open("cachelib/__init__.py", "rt", encoding="utf8") as f:
-    version = re.search(r"__version__ = \'(.*?)\'", f.read()).group(1)
-
-
-setup(
-    name="cachelib",
-    version=version,
-    url="https://github.com/pallets/cachelib",
-    license="BSD",
-    author="Pallets team",
-    author_email="contact@palletsprojects.com",
-    description="A collection of cache libraries in the same API interface.",
-    long_description=readme,
-    packages=["cachelib"],
-    include_package_data=True,
-    zip_safe=False,
-    platforms="any",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-)
+# Metadata in setup.cfg
+setup(name="cachelib", version=version)
