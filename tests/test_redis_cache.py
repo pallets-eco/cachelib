8e55c28b315a83953bc66f9be39f6603534fbbc2
northernSage <gfvante@gmail.com>
2021-05-22 19:12:54 -0300

    add redis tests

---
+++ tests/test_redis_cache.py
@@ -0,0 +1,30 @@
+import pytest
+
+from conftest import CommonTests
+from cachelib import RedisCache
+
+
+@pytest.fixture(autouse=True)
+def cache_factory(request):
+    def _factory(self, *args, **kwargs):
+        rc = RedisCache(*args, **kwargs)
+        rc._client.flushdb()
+        return rc
+
+    request.cls.cache_factory = _factory
+
+
+@pytest.mark.usefixtures("redis_server")
+class TestRedisCache(CommonTests):
+    def test_has(self):
+        cache = self.cache_factory()
+        assert cache.set_many(self.sample_pairs)
+        for k in self.sample_pairs:
+            assert cache.has(k)
+        assert not cache.has("unknown")
+
+    def test_clear(self):
+        cache = self.cache_factory()
+        assert cache.set_many(self.sample_pairs)
+        assert cache.clear()
+        assert not any(cache.get_many(*self.sample_pairs))
