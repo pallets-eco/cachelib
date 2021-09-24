c11120cde30ea5ff7c38ddb274ad807fc4a35497
Myrik <myrik260138@tut.by>
2021-09-25 01:03:42 +0200

    add test

--- tests/test_redis_cache.py
+++ tests/test_redis_cache.py
@@ -1,3 +1,5 @@
+import pickle
+
 import pytest
 from clear import ClearTests
 from common import CommonTests
@@ -18,4 +20,37 @@ def cache_factory(request):
 
 @pytest.mark.usefixtures("redis_server")
 class TestRedisCache(CommonTests, ClearTests, HasTests):
-    pass
+    def test_dump_object(self):
+        cache = self.cache_factory()
+
+        expected = {
+            4: {
+                "128": b"!\x80\x04\x89.",
+                "beef": b"!\x80\x04\x88.",
+                "crevettes": b"!\x80\x04}\x94.",
+                "1024": b"!\x80\x04\x95\x08\x00\x00\x00\x00\x00\x00\x00\x8c\x04spam\x94.",
+                "bacon": b"!\x80\x04\x95\x08\x00\x00\x00\x00\x00\x00\x00\x8c\x04eggs\x94.",
+                "sausage": b"2048",
+                "3072": b"!\x80\x04]\x94.",
+                "brandy": b"!\x80\x04\x95\x14\x00\x00\x00\x00\x00\x00\x00]\x94(}\x94\x8c\nfried eggs\x94e.",
+                "lobster": b"!\x80\x04\x95\x19\x00\x00\x00\x00\x00\x00\x00]\x94(\x8c\x0bbaked beans\x94]\x94M\x00\x02ae.",
+                "4096": b"!\x80\x04\x95\x1c\x00\x00\x00\x00\x00\x00\x00}\x94(\x8c\x05sauce\x94]\x94M\x00\x01\x8c\x07truffle\x94u.",
+            },
+            5: {
+                "128": b"!\x80\x05\x89.",
+                "beef": b"!\x80\x05\x88.",
+                "crevettes": b"!\x80\x05}\x94.",
+                "1024": b"!\x80\x05\x95\x08\x00\x00\x00\x00\x00\x00\x00\x8c\x04spam\x94.",
+                "bacon": b"!\x80\x05\x95\x08\x00\x00\x00\x00\x00\x00\x00\x8c\x04eggs\x94.",
+                "sausage": b"2048",
+                "3072": b"!\x80\x05]\x94.",
+                "brandy": b"!\x80\x05\x95\x14\x00\x00\x00\x00\x00\x00\x00]\x94(}\x94\x8c\nfried eggs\x94e.",
+                "lobster": b"!\x80\x05\x95\x19\x00\x00\x00\x00\x00\x00\x00]\x94(\x8c\x0bbaked beans\x94]\x94M\x00\x02ae.",
+                "4096": b"!\x80\x05\x95\x1c\x00\x00\x00\x00\x00\x00\x00}\x94(\x8c\x05sauce\x94]\x94M\x00\x01\x8c\x07truffle\x94u.",
+            },
+        }[pickle.DEFAULT_PROTOCOL]
+
+        for k, v in self.sample_pairs.items():
+            dumped = cache.dump_object(v)
+            exp = expected.get(k)
+            assert dumped == exp
